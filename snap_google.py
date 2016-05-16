961ee5442238ed2e2f5b2e8284d78a184a85d642
rAnYKM <jcjy21@gmail.com>
2016-05-15 17:02:22 -0700

    GEXF File generator
    finish function to generate .gexf files

--- snap_google.py
+++ snap_google.py
@@ -11,8 +11,9 @@
 # Date: May. 9, 2016
 
 import os
-from ranfig import load_ranfig
 import logging
+import networkx as nx
+from ranfig import load_ranfig
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -46,6 +47,22 @@ class SnapEgoNet:
         index = [num for num, value in enumerate(fea) if value == '1']
         return uid, index
 
+    def __build_network(self):
+        ego_net = nx.DiGraph()
+        ego_net.add_edges_from(self.edges)
+        ego_edges = [(self.root, vertex) for vertex in self.node.keys()]
+        # Note: Because we DO NOT HAVE the feature list of followers, we comment this part
+        # ego_edges += [(vertex, self.root) for vertex in self.followers]
+        ego_net.add_edges_from(ego_edges)
+        logging.debug('%d Nodes, %d Edges in the ego network.'
+                      % (ego_net.number_of_nodes(), ego_net.number_of_edges()))
+        return ego_net
+
+    def __check_whether_in_node_feat(self, id_list):
+        not_in_it = [vertex for vertex in id_list if vertex not in self.node]
+        logging.debug('%d follower(s) not in the node feat list' % len(not_in_it))
+        return not_in_it
+
     def __feat_name_list(self):
         with open(os.path.join(self.dir['SNAP'], self.root + '.featnames'), 'rb') as fp:
             feat_name = [self.__feat_process(line) for line in fp.readlines()]
@@ -61,9 +78,10 @@ class SnapEgoNet:
 
     def __ego_feat_list(self):
         with open(os.path.join(self.dir['SNAP'], self.root + '.egofeat'), 'rb') as fp:
-            id, node = self.__node_process(fp.readline())
-            logging.debug('%d Ego Feature(s) have been loaded.' % len(node))
-            return node
+            li = fp.readline().strip('\r\n').split(' ')
+            index = [num for num, value in enumerate(li) if value == '1']
+            logging.debug('%d Ego Feature(s) have been loaded.' % len(index))
+            return index
 
     def __edge_list(self):
         with open(os.path.join(self.dir['SNAP'], self.root + '.edges'), 'rb') as fp:
@@ -84,6 +102,98 @@ class SnapEgoNet:
             logging.debug('%d Ego Follower(s) have been loaded.' % len(followers))
             return followers
 
+    def get_ego_features(self):
+        ego_features = [self.featname[feat] for feat in self.egofeat]
+        return ego_features
+
+    def get_network(self, mode='follow', label_with_feature='job_title'):
+        """
+        return a directed network with specific mode and labels
+        :param mode: String 'friend', 'follow'
+        :param label_with_feature: String Feature Category 'gender', 'institution', 'job_title', 'university', 'place'
+        :return: nx.DiGraph
+        """
+        if mode == 'follow':
+            network = self.network
+            labels = list()
+            for node in network.nodes_iter():
+                if node == self.root:
+                    feature = self.egofeat
+                else:
+                    feature = self.node[node]
+                lab = [self.featname[f][0] for f in feature if self.featname[f][1] == label_with_feature]
+                if len(lab) == 0:
+                    lab = ['unlabeled']
+                labels.append((node, {label_with_feature: ' '.join(lab)}))
+            network.add_nodes_from(labels)
+            nx.write_gexf(network, os.path.join(self.dir['OUT'], self.root + '-ego-follow.gexf'))
+            logging.debug('Network Generated in %s' % os.path.join(self.dir['OUT'], self.root + '-ego-follow.gexf'))
+        elif mode == 'friend':
+            network = nx.DiGraph()
+            vertices = self.followers + [self.root]
+            edges = [edge for edge in self.network.edges_iter()
+                     if edge[0] in vertices and edge[1] in vertices]
+            network.add_edges_from(edges)
+            labels = list()
+            for node in network.nodes_iter():
+                if node == self.root:
+                    feature = self.egofeat
+                else:
+                    feature = self.node[node]
+                lab = [self.featname[f][0] for f in feature if self.featname[f][1] == label_with_feature]
+                if len(lab) == 0:
+                    lab = ['unlabeled']
+                labels.append((node, {label_with_feature: ' '.join(lab)}))
+            network.add_nodes_from(labels)
+            nx.write_gexf(network, os.path.join(self.dir['OUT'], self.root + '-ego-friend.gexf'))
+            logging.debug('Network Generated in %s' % os.path.join(self.dir['OUT'], self.root + '-ego-friend.gexf'))
+
+    def get_binary_label_network(self, features, mode='follow'):
+        """
+        return a directed network with specific mode and binary labels
+        :param mode: String 'friend', 'follow'
+        :param features: List of Int feature sequence numbers
+        :return: nx.DiGraph
+        """
+        if mode == 'follow':
+            network = self.network
+            labels = list()
+            for node in network.nodes_iter():
+                if node == self.root:
+                    feature = self.egofeat
+                else:
+                    feature = self.node[node]
+                flag = False
+                for f in feature:
+                    if f in features:
+                        flag = True
+                        break
+                labels.append((node, {'Binary Label': flag}))
+            network.add_nodes_from(labels)
+            nx.write_gexf(network, os.path.join(self.dir['OUT'], self.root + '-ego-follow.gexf'))
+            logging.debug('Network Generated in %s' % os.path.join(self.dir['OUT'], self.root + '-ego-follow.gexf'))
+        elif mode == 'friend':
+            network = nx.DiGraph()
+            vertices = self.followers + [self.root]
+            edges = [edge for edge in self.network.edges_iter()
+                     if edge[0] in vertices and edge[1] in vertices]
+            network.add_edges_from(edges)
+            labels = list()
+            for node in network.nodes_iter():
+                if node == self.root:
+                    feature = self.egofeat
+                else:
+                    feature = self.node[node]
+                flag = False
+                for f in feature:
+                    if f in features:
+                        flag = True
+                        break
+                labels.append((node, {'Binary Label': flag}))
+            network.add_nodes_from(labels)
+            nx.write_gexf(network, os.path.join(self.dir['OUT'], self.root + '-ego-friend.gexf'))
+            logging.debug('Network Generated in %s' % os.path.join(self.dir['OUT'], self.root + '-ego-friend.gexf'))
+
     def __init__(self, ego_id):
         self.dir = load_ranfig()
         self.root = ego_id
@@ -92,11 +202,16 @@ class SnapEgoNet:
         self.egofeat = self.__ego_feat_list()
         self.edges, self.follows = self.__edge_list()
         self.followers = self.__follower_list()
+        self.network = self.__build_network()
+        # not_in_it = self.__check_whether_in_node_feat(self.followers)
+        # not_in_it = self.__check_whether_in_node_feat(self.follows)
 
 
 def main():
-    user = SnapEgoNet('100129275726588145876')
-
+    user = SnapEgoNet('100535338638690515335')
+    print(user.get_ego_features())
+    # user.get_network(mode='friend', label_with_feature='job_title')
+    user.get_binary_label_network(features=range(38, 45, 1), mode='friend')
 
 if __name__ == '__main__':
     main()
