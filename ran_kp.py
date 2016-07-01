3f6c0bd65227d90dd2060432128d0000600749ef
rAnYKM <jcjy21@gmail.com>
2016-06-30 17:34:08 -0700

    branch and bound

--- ran_kp.py
+++ ran_kp.py
@@ -30,14 +30,15 @@ class Knapsack:
         self.sorted_items = sorted(self.items, key=lambda tup: float(tup[1])/tup[2], reverse=True)
 
     def dp_solver(self):
-        def dp_recursive(i, j):
-            if i == 0: return 0
-            id, value, weight = self.items[i - 1]
-            if weight > j:
-                return dp_recursive(i - 1, j)
+        def dp_recursive(p, q):
+            if p == 0:
+                return 0
+            _, value, weight = self.items[p - 1]
+            if weight > q:
+                return dp_recursive(p - 1, q)
             else:
-                return max(dp_recursive(i - 1, j),
-                           dp_recursive(i - 1, j - weight) + value)
+                return max(dp_recursive(p - 1, q),
+                           dp_recursive(p - 1, q - weight) + value)
 
         j = self.max_weight
         n = len(self.items)
@@ -45,15 +46,63 @@ class Knapsack:
         for i in xrange(n, 0, -1):
             if dp_recursive(i, j) != dp_recursive(i - 1, j):
                 result.append(self.items[i - 1])
-                j -= self.items[i - 1][1]
+                j -= self.items[i - 1][2]
         result.reverse()
         return dp_recursive(n, self.max_weight), result
 
+    def bnb_solver(self):
+        class Node:
+            def __init__(self, level, weight, value):
+                self.level = level
+                self.weight = weight
+                self.value = value
+                self.bound = 0
+                self.sel = []
+
+            def get_bound(self, items, max_weight):
+                if self.weight >= max_weight:
+                    return 0
+                p_bound = self.value
+                j = self.level + 1
+                w = self.weight
+                while j < len(items) and w + items[j][2] <= max_weight:
+                    w += items[j][2]
+                    p_bound += items[j][1]
+                    j += 1
+                if j < len(items):
+                    p_bound += (max_weight - w)*items[j][1]/float(items[j][2])
+                self.bound = p_bound
+
+        # [level, weight, value]
+        u = Node(-1, 0, 0)
+        v = Node(0, 0, 0)
+        queue = [u]
+        max_value = 0
+        max_sel = []
+        while len(queue) != 0:
+            u = queue.pop(0)
+            if u.level == len(self.sorted_items) - 1:
+                continue
+            v.level = u.level + 1
+            v.weight = u.weight + self.sorted_items[v.level][2]
+            v.value = u.value + self.sorted_items[v.level][1]
+            if v.weight <= self.max_weight and v.value > max_value:
+                v.sel.append(self.sorted_items[v.level])
+                max_value = v.value
+                max_sel = v.sel
+            v.get_bound(self.sorted_items, self.max_weight)
+            if v.bound > max_value:
+                queue.append(v)
+        return max_value, max_sel
+
+
 def test():
-    items = [(0, 4, 12), (1, 2, 1), (2, 6, 4), (3, 1, 1), (4, 2, 2)]
-    kp = Knapsack(items, 15)
+    # items = [(0, 4, 12), (1, 2, 1), (2, 6, 4), (3, 1, 1), (4, 2, 2)]
+    items = [(0, 40, 2), (1, 50, 3.14), (2, 100, 1.98), (3, 95, 5), (4, 30, 3)]
+    kp = Knapsack(items, 10)
     print kp.sorted_items
     print kp.dp_solver()
+    print kp.bnb_solver()
 
 if __name__ == '__main__':
     test()
